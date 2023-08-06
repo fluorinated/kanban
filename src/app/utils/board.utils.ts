e7782eeefc94b1449c9336d154f0a6e7b45c0c16
fluorinated <kneonxknight@yahoo.com>
2023-08-06 15:18:52 -0400

    fix due this week and month filters

--- src/app/utils/board.utils.ts
+++ src/app/utils/board.utils.ts
@@ -59,38 +59,31 @@ export const dueTodayTickets = (filteredTickets: Ticket[]) => {
 };
 
 export const dueThisWeekTickets = (filteredTickets: Ticket[]) => {
-  var startOfWeek = moment().startOf('week').toDate();
-  var endOfWeek = moment().endOf('week').toDate();
+  var startOfWeek = moment().startOf('week');
+  var endOfWeek = moment().endOf('week');
+
   filteredTickets = filteredTickets.filter((ticket) => {
     let ticketDueDate = new Date(ticket.dueDate);
-    let ticketCurrentDay = String(ticketDueDate.getDate()).padStart(2, '0');
-    let ticketCurrentMonth = String(ticketDueDate.getMonth() + 1).padStart(
-      2,
-      '0'
-    );
-    let ticketCurrentYear = ticketDueDate.getFullYear();
-    let ticketCurrentDate = `${ticketCurrentDay}-${ticketCurrentMonth}-${ticketCurrentYear}`;
-
-    return moment(ticketCurrentDate).isBetween(startOfWeek, endOfWeek);
+    return moment(ticketDueDate).isBetween(startOfWeek, endOfWeek, null, '[]');
   });
+
   return filteredTickets;
 };
 
 export const dueThisMonthTickets = (filteredTickets: Ticket[]) => {
-  var startOfMonth = moment().startOf('month').toDate();
-  var endOfMonth = moment().endOf('month').toDate();
+  var startOfMonth = moment().startOf('month');
+  var endOfMonth = moment().endOf('month');
+
   filteredTickets = filteredTickets.filter((ticket) => {
     let ticketDueDate = new Date(ticket.dueDate);
-    let ticketCurrentDay = String(ticketDueDate.getDate()).padStart(2, '0');
-    let ticketCurrentMonth = String(ticketDueDate.getMonth() + 1).padStart(
-      2,
-      '0'
+    return moment(ticketDueDate).isBetween(
+      startOfMonth,
+      endOfMonth,
+      null,
+      '[]'
     );
-    let ticketCurrentYear = ticketDueDate.getFullYear();
-    let ticketCurrentDate = `${ticketCurrentDay}-${ticketCurrentMonth}-${ticketCurrentYear}`;
-
-    return moment(ticketCurrentDate).isBetween(startOfMonth, endOfMonth);
   });
+
   return filteredTickets;
 };
 
